166821866a88eec77e1aefefc3d5d1bcd7e4d86e
Mt Matthew Whittle Cell <mattdwhittle@gmail.com>
2018-12-02 17:34:30 -0800

    Initial Commit

---
+++ eScan for Root LabTests/eScan_for_Root_LabTests.swift
@@ -0,0 +1,34 @@
+//
+//  eScan_for_Root_LabTests.swift
+//  eScan for Root LabTests
+//
+//  Created by RahMat on 12/2/18.
+//  Copyright © 2018 rootlab. All rights reserved.
+//
+
+import XCTest
+@testable import eScan_for_Root_Lab
+
+class eScan_for_Root_LabTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
